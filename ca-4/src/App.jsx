3ebf21b5310687ade3f9420635cef91d054972db
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2023-12-22 16:30:17 +0530

    hello

--- ca-4/src/App.jsx
+++ ca-4/src/App.jsx
@@ -1,28 +1,50 @@
+
 import React, { useState } from "react";
-import Quiz from "./Components/Quiz"; 
-import "./App.css"
+import Quiz from "./Components/Quiz";
+import "./App.css";
 
+// Main App component
 function App() {
-  const [dark,setDark] = useState(false);
-  
+  // State to enable dark mode
+  const [dark, setDark] = useState(false);
 
-  const toggleDarkMode=()=>{
-    setDark(!dark)
-  }
+  // Function to cahnge or rather play with dark and light modes
+  const toggleDarkMode = () => {
+    setDark(!dark);
+  };
+
+  // Rendering the App component
   return (
-    <div className="app" style={{backgroundColor: dark ? "#D9D9D9" : "#02040F"}}>
+    <div className="app" style={{ backgroundColor: dark ? "#D9D9D9" : "#02040F" }}>
+      {/* Navbar section */}
       <div className="navbar">
         <h3 className="logo">Kalvium</h3>
-        <button className="theme" style={{ backgroundColor: dark ? "#D9D9D9" : "#02040F",
-        color: dark ? "#000" : "#E5DADA",
-       }}
-          onClick={toggleDarkMode}> {dark ? "LIGHT" : "DARK"}</button>
+        {/* Button to toggle dark mode */}
+        <button
+          className="theme"
+          style={{
+            backgroundColor: dark ? "#D9D9D9" : "#02040F",
+            color: dark ? "#000" : "#E5DADA",
+          }}
+          onClick={toggleDarkMode}
+        >
+          {dark ? "LIGHT" : "DARK"}
+        </button>
       </div>
-      <div className="questions" style={{backgroundColor:dark? "rgba(2, 4, 15, 0.5)" : "rgba(229, 218, 218, 0.7)"}}>
-        <Quiz /> 
+
+      {/* Questions section containing the Quiz component */}
+      <div
+        className="questions"
+        style={{
+          backgroundColor: dark ? "rgba(2, 4, 15, 0.5)" : "rgba(229, 218, 218, 0.7)",
+        }}
+      >
+        {/* Render the Quiz component */}
+        <Quiz />
       </div>
     </div>
   );
 }
 
+// Exporting the App component as the default export
 export default App;
--- ca-4/src/Components/Quiz.jsx
+++ ca-4/src/Components/Quiz.jsx
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-import Questions from "./Questions"; 
-import Result from "./Results"; 
 
+import React, { useState } from "react";
+import Questions from "./Questions";
+import Result from "./Results";
+
+// Main Quiz component
 function Quiz() {
+  // to get or rather enable these given below
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
-  const [ishighlight, setisHighlight] = useState(false)
+  const [ishighlight, setisHighlight] = useState(false);
 
+  // Function to handle answer option
   const handleAnswerClick = (selectedOption) => {
+    // Check if the selected option is correct and update the score
     if (selectedOption === Questions[currentQuestionIndex].correctAnswer) {
       setScore(score + 1);
     }
 
-    // Move to the next question or show the score
+    // Move to the next question or show the score if it's the last question here we are using if condition
     if (currentQuestionIndex + 1 < Questions.length) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
@@ -21,23 +26,29 @@ function Quiz() {
     }
   };
 
+  // to restart the quiz
   const restartQuiz = () => {
     setCurrentQuestionIndex(0);
     setScore(0);
     setShowScore(false);
   };
 
+  // Rendering the Quiz component
   return (
     <div className="quiz-container">
       {showScore ? (
+        // Display the Result component if the quiz is completed
         <Result score={score} totalQuestions={Questions.length} restartQuiz={restartQuiz} />
       ) : (
+        // Display the current question and options
         <div className="question-section">
           <h2 className="heading">Question {currentQuestionIndex + 1} out of {Questions.length}</h2>
-          <p className="qn" style={{
-            color:`${ishighlight?"#C5001A":"white"}`
-          }}>{Questions[currentQuestionIndex].text}</p>
+        
+          <p className="qn" style={{ color: `${ishighlight ? "#C5001A" : "white"}` }}>
+            {Questions[currentQuestionIndex].text}
+          </p>
           <div className="options">
+            {/* Map through and display answer options as buttons */}
             {Questions[currentQuestionIndex].options.map((option) => (
               <button
                 key={option.id}
@@ -46,15 +57,25 @@ function Quiz() {
               >
                 {option.text}
               </button>
-              
             ))}
+            {/* Highlight control buttons */}
             <div className="highlights">
-                <button onClick={()=>{
-                    setisHighlight(true)
-                }} className="highbtn">HIGHLIGHT</button>
-                <button onClick={()=>{
-                    setisHighlight(false)
-                }} className="highbtn">REMOVE HIGHLIGHT</button>
+              <button
+                onClick={() => {
+                  setisHighlight(true);
+                }}
+                className="highbtn"
+              >
+                HIGHLIGHT
+              </button>
+              <button
+                onClick={() => {
+                  setisHighlight(false);
+                }}
+                className="highbtn"
+              >
+                REMOVE HIGHLIGHT
+              </button>
             </div>
           </div>
         </div>
@@ -63,4 +84,5 @@ function Quiz() {
   );
 }
 
+// Exporting the Quiz inside component file
 export default Quiz;
